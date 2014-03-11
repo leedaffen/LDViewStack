97ef0f8acb155158f72d5f84674e6155c11a4168
Lee Daffen <leedaffen@hotmail.com>
2014-03-11 21:06:43 +0000

    updated comments

--- Classes/LDViewStack.h
+++ Classes/LDViewStack.h
@@ -37,18 +37,36 @@
 
 @property (nonatomic, weak) IBOutlet id<LDViewStackDataSource> dataSource;
 @property (nonatomic, weak) IBOutlet id<LDViewStackDelegate> delegate;
+
+
+/*
+ * Duration of the shuffle animation
+ * This triggers after dragging ends and slides the selected view to the bottom of the stack
+ */
 @property (nonatomic, assign) CGFloat shuffleAnimationDuration;
 
+/*
+ * Controls the number of 'visible' views
+ * If set to 1 only the top-most view is displayed
+ * Defaults to 3
+ */
 @property (nonatomic, assign) NSUInteger maxVisibleItems;
 
-// Allow translations in X and Y axis. Both default to YES;
+/*
+ * Prevent translations in X and/or Y axis
+ * Both default to NO
+ */
 @property (nonatomic, assign) BOOL preventX;
 @property (nonatomic, assign) BOOL preventY;
 
-// OPTIONAL - view into which the stack will be temporarily placed when dragging (used prevent bounds-clipping from parent views)
+/*
+ * OPTIONAL - view into which the stack will be temporarily placed when dragging (used prevent bounds-clipping from parent views)
+ */
 @property (nonatomic, weak) UIView *overlayParentView;
 
-
+/*
+ * Update the contents of the stack from the data source
+ */
 - (void)reloadData;
 
 @end
--- Classes/LDViewStackView.h
+++ Classes/LDViewStackView.h
@@ -5,6 +5,9 @@
 //  Created by Lee Daffen on 09/05/2013.
 //  Copyright (c) 2013 Lee Daffen. All rights reserved.
 //
+//
+// Internal container view implementation
+//
 
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
